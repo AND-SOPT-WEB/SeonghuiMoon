f2cc4dfd8b2c3d4a211610fa5f9790d75320fa0e
moonseonghui <msh9980@naver.com>
2024-10-27 02:42:43 +0900

    feat: 깃허브 링크 구현

--- week2/javascripts/renderTable.js
+++ week2/javascripts/renderTable.js
@@ -10,7 +10,9 @@ const renderTable = () => {
       <td class="bodyCell"><input type="checkbox" /></td>
       <td class="bodyCell">${member.name}</td>
       <td class="bodyCell">${member.englishName}</td>
-      <td class="bodyCell">${member.github}</td>
+      <td class="bodyCell">
+        <a href="https://github.com/${member.github}" target="_blank">${member.github}</a>
+      </td>
       <td class="bodyCell">${member.gender === "male" ? "남자" : "여자"}</td>
       <td class="bodyCell">${member.role}</td>
       <td class="bodyCell">${member.firstWeekGroup}</td>
