a465335ebae8948557c1024579ffa580ef86de34
moonseonghui <msh9980@naver.com>
2024-10-29 01:49:05 +0900

    feat(modal): modal 열기, 닫기 동작 구현

--- week2/javascripts/modal.js
+++ week2/javascripts/modal.js
@@ -1,16 +1,15 @@
-const modal = document.querySelector(".addModal");
-const modalBackground = document.querySelector(".modalBackground");
-
 const openModal = () => {
   modal.style.display = "block";
   modalBackground.style.display = "block";
 };
 
-const closeModal = () => {
+export const closeModal = () => {
   modal.style.display = "none";
   modalBackground.style.display = "none";
 };
 
+const modal = document.querySelector(".addModal");
+const modalBackground = document.querySelector(".modalBackground");
 const addButton = document.querySelector(".addBtn");
 const closeModalBtn = document.querySelector(".closeModalBtn");
 
