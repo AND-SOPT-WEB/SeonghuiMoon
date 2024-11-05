ea69e002b78767593f9b22f5f1a322fe817166d7
moonseonghui <msh9980@naver.com>
2024-11-05 20:23:45 +0900

    feat: 페이지 라우팅 구현

--- week3/src/App.jsx
+++ week3/src/App.jsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Mainpage from "./pages/Mainpage";
+import RankingPage from "./pages/RankingPage";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Mainpage />} />
+        <Route path="/ranking" element={<RankingPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- week3/src/main.jsx
+++ week3/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import GlobalStyle from "./styles/globalStyle.js";
+import GlobalStyle from "./styles/GlobalStyle.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
