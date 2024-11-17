66e2aeae4f55fc3740779456a1fcd535e64c2132
moonseonghui <msh9980@naver.com>
2024-11-18 02:03:32 +0900

    feat: page routing

--- week4/src/App.tsx
+++ week4/src/App.tsx
@@ -1,11 +1,20 @@
-import styled from "@emotion/styled";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginPage />,
+  },
+  {
+    path: "/home",
+    element: <HomePage />,
+  },
+]);
 
 function App() {
-  return <Test>dd</Test>;
+  return <RouterProvider router={router} />;
 }
-
-const Test = styled.div`
-  font-size: 1rem;
-`;
 
 export default App;
