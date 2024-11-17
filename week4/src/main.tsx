8410f3fe9affa86d0faabc47c74aa30f83e37500
moonseonghui <msh9980@naver.com>
2024-11-18 01:56:40 +0900

    feat: theme 설정

--- week4/src/main.tsx
+++ week4/src/main.tsx
@@ -2,10 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import GlobalStyle from "./styles/GlobalStyle.tsx";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./styles/Theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <GlobalStyle />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
---
+++ week4/src/styles/Theme.ts
@@ -0,0 +1,6 @@
+const COLORS = {
+  BLACK: "#000000",
+  WHITE: "#FFFFFF",
+};
+
+export default COLORS;
