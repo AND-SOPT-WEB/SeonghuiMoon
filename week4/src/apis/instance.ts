01f5c3350565b65d703f9471c9c1aac1915f57ae
moonseonghui <msh9980@naver.com>
2024-11-23 14:20:07 +0900

    feat: authAxiso interceptors 추가

--- week4/src/apis/instance.ts
+++ week4/src/apis/instance.ts
@@ -13,6 +13,18 @@ export const authInstance = axios.create({
   baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
-    token: `${localStorage.getItem("token")}`,
   },
 });
+
+authInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.token = token;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
