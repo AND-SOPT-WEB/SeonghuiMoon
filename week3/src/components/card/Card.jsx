f50cac1570f5065ee9c17448d9df19192405105c
moonseonghui <msh9980@naver.com>
2024-12-18 19:16:37 +0900

    feat: 카드 클릭 시 깜박거리는 효과 추가

--- week3/src/components/card/Card.jsx
+++ week3/src/components/card/Card.jsx
@@ -1,8 +1,33 @@
-import styled from "styled-components";
+import styled, { keyframes, css } from "styled-components";
+import { useState } from "react";
 
 const Card = ({ number, onClick }) => {
-  return <CardStyled onClick={onClick}>{number}</CardStyled>;
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(true);
+
+    setTimeout(() => {
+      setIsClicked(false);
+      if (onClick) onClick();
+    }, 300);
+  };
+
+  return (
+    <CardStyled onClick={handleClick} isClicked={isClicked}>
+      {number}
+    </CardStyled>
+  );
 };
+
+const blink = keyframes`
+  0%, 100% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.2;
+  }
+`;
 
 const CardStyled = styled.div`
   width: 10rem;
@@ -10,9 +35,16 @@ const CardStyled = styled.div`
   background-color: darkcyan;
 
   font-size: 4rem;
-  align-content: center;
-  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   color: white;
+
+  ${({ isClicked }) =>
+    isClicked &&
+    css`
+      animation: ${blink} 0.3s linear;
+    `}
 `;
 
 export default Card;
